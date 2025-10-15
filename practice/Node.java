7c2bafb1694c91983bc2ea891944597b731328e8
rgupta11 <ravigupta.rbl@gmail.com>
2025-10-15 17:50:46 +0530

    Add files via upload

---
+++ practice/Node.java
@@ -0,0 +1,37 @@
+package com.rga.ds.bst;
+
+public class Node {
+    private int element;
+    private Node left;
+    private Node right;
+    public Node(int element) {
+        this.element = element;
+        this.left = null;
+        this.right = null;
+    }
+
+    // Getters and Setters
+    public int getElement() {
+        return element;
+    }
+
+    public void setElement(int element) {
+        this.element = element;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
